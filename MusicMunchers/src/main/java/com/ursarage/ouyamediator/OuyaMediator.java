c749cb0fbe4225662c3ef165951aebabacd33e3b
SeriousHornCat <angelina.antonison@gmail.com>
2013-07-04 16:10:02 -0500

    Adding the initial controller listener prototype.

--- MusicMunchers/src/main/java/com/ursarage/ouyamediator/OuyaMediator.java
+++ MusicMunchers/src/main/java/com/ursarage/ouyamediator/OuyaMediator.java
@@ -3,6 +3,7 @@ package com.ursarage.ouyamediator;
 import android.content.Context;
 
 import tv.ouya.console.api.OuyaFacade;
+import IOuyaControllerListener;
 
 /**
  * Created by Angelina on 7/4/13.
@@ -11,9 +12,20 @@ public class OuyaMediator {
 
     public static final String DEVELOPER_ID="";
     OuyaFacade mOuya;
+
+    List<IOuyaControllerListener> controllerLisetners;
     public OuyaMediator(Context gameContext) {
         mOuya = OuyaFacade.getInstance();
         mOuya.init(gameContext,DEVELOPER_ID );
-
+        controllerLisetners = new List<IOuyaControllerListener>();
     }
+
+    public boolean onKeyDown(int keyCode, android.view.KeyEvent event)
+    {
+        for(Iterator<IOuyaControllerListener> i = controllerLisetners.iterator(); i.hasNext(); ) {
+            IOuyaControllerListener item = i.next();
+            item.onKeyEvent(keyCode, event);
+        }
+    }
+
 }
