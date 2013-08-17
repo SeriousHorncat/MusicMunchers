9c16c758a0a7d85a799069d42eed13c628b2198c
SeriousHorncat <angelina.antonison@gmail.com>
2013-08-17 08:46:21 -0500

    Removed the IOuyaControllerListener

--- MusicMunchers/src/main/java/com/ursarage/musicmunchers/Gameboard.java
+++ MusicMunchers/src/main/java/com/ursarage/musicmunchers/Gameboard.java
@@ -9,13 +9,12 @@ import org.andengine.entity.sprite.Sprite;
 import android.util.Log;
 import android.view.KeyEvent;
 
-import com.ursarage.ouyamediator.IOuyaControllerListener;
 import com.ursarage.toolkit.Point;
 import com.ursarage.toolkit.Vector2;
 
 import tv.ouya.console.api.OuyaController;
 
-public class Gameboard extends Rectangle implements MusicMuncherDefines, IOuyaControllerListener {
+public class Gameboard extends Rectangle implements MusicMuncherDefines {
 
 
 	int mNumberOfColumns = 0;
@@ -23,6 +22,7 @@ public class Gameboard extends Rectangle implements MusicMuncherDefines, IOuyaCo
 	
 	String mScale;
 	private int mTotalNoteCount = 0;
+
 	
 	BoardPiece[][] pieces;
 	
@@ -31,12 +31,15 @@ public class Gameboard extends Rectangle implements MusicMuncherDefines, IOuyaCo
 	GameLevelScene mLevel;
 	private final Random random = new Random();
     private Point mBoardPosition;
+    private Vector2 mMuncherBoardLocation;
+    private boolean isReady = false;
 	
 	public Gameboard(GameLevelScene level, Camera camera, String scale, Vector2 allocatedBoardAreaSize) {
 		super(0,0, camera.getSurfaceX(), camera.getSurfaceY(), level.vertexBufferObjectManager);
 		
 		mLevel = level;
 		mScale = scale;
+        mMuncherBoardLocation = new Vector2(0,0);
 
         mNumberOfColumns = (allocatedBoardAreaSize.X / CELL_WIDTH) - 2;
 		mNumberOfRows = (allocatedBoardAreaSize.Y / CELL_HEIGHT) -1;
@@ -47,9 +50,11 @@ public class Gameboard extends Rectangle implements MusicMuncherDefines, IOuyaCo
         {
             for(int j = 0; j < mNumberOfRows; j++)
             {
+              Log.d("touch", "Creating piece at " + i + "," + j );
               String note = chooseNote( scale );
               BoardPiece piece = new BoardPiece(note, 0, 0, mLevel.resourcesManager.mBoardCell,
-                      mLevel.resourcesManager.levelText, this, mLevel.vertexBufferObjectManager);
+                      mLevel.resourcesManager.levelText, this, mLevel.vertexBufferObjectManager,
+                      new Vector2(i, j));
               pieces[i][j] = piece;
               this.attachChild(piece);
               level.registerTouchArea(piece);
@@ -69,8 +74,11 @@ public class Gameboard extends Rectangle implements MusicMuncherDefines, IOuyaCo
         this.setBoardPosition(mBoardPosition.X, mBoardPosition.Y);
         Log.d("touch", "made board pieces...");
 
-		this.moveMuncher( randomBoardPiece() );
+        this.moveMuncher( randomBoardPiece() );
         Log.d("touch", "moved the muncher");
+
+        calculateNumberOfCorrectNote();
+        isReady = true;
 	}
 
 	public final int score() {
@@ -94,28 +102,13 @@ public class Gameboard extends Rectangle implements MusicMuncherDefines, IOuyaCo
 	
 	public void onTouchEvent( BoardPiece pieceTouched ) {
 		if( this.isLocationSame(mLevel.mMuncher, pieceTouched) && !pieceTouched.isClear() ) {
-			Log.w("touch", "Was same spot");
-			Log.w("touch", "ScaleName" + mScale + " and board text " + pieceTouched.text() );
-			if( ScaleRepository.doesScaleContains(mScale, pieceTouched.text()) )	{
-				this.awardPoints();
-				Log.w("touch", "awwarded points");
-				mTotalNoteCount -= 1;
-				Log.w("touch", "Notes Left: " + mTotalNoteCount);
-			}			
-			else{
-				Log.w("touch", "removed points");
-				this.removePoints();
-			}
-			
-			mLevel.updateScoreDisplay();
-			
-			pieceTouched.clear();
-			if( mTotalNoteCount <= 0 )
+            Log.w("touch", "Was same spot");
+            eatNote(pieceTouched);
+			if( isLevelComplete() )
 			{
 				Log.w("touch", "GAME OVER!!!");
 				mLevel.levelComplete();
 			}
-				
 		}
 
 		if( this.isAdjacent(mLevel.mMuncher, pieceTouched) )
@@ -124,20 +117,95 @@ public class Gameboard extends Rectangle implements MusicMuncherDefines, IOuyaCo
 		}
 	}
 
+    public int calculateNumberOfCorrectNote() {
+        for( int i = 0; i < this.mNumberOfColumns; i++ )
+        {
+            for(int j = 0; j < mNumberOfRows; j++)
+            {
+                if( ScaleRepository.doesScaleContains(mScale, pieces[i][j].mNote) )
+                    mTotalNoteCount +=1;
+            }
+        }
 
-    @Override
+       return mTotalNoteCount;
+    }
+
+    private boolean isLevelComplete() {
+        return  (mTotalNoteCount <= 0);
+    }
+
+    private void eatNote(BoardPiece piece) {
+        Log.w("touch", "ScaleName" + mScale + " and board text " + piece.text() );
+        if( ScaleRepository.doesScaleContains(mScale, piece.text()) )	{
+            this.awardPoints();
+            Log.w("touch", "awwarded points");
+            mTotalNoteCount -= 1;
+            Log.w("touch", "Notes Left: " + mTotalNoteCount);
+        }
+        else{
+            Log.w("touch", "removed points");
+            this.removePoints();
+        }
+
+        mLevel.updateScoreDisplay();
+        piece.clear();
+    }
+
+
     public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if( SceneManager.getInstance().getCurrentSceneType() != mLevel.getSceneType()) {
+            return false;
+        }
+        return false;
+    }
+
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if( SceneManager.getInstance().getCurrentSceneType() != mLevel.getSceneType()) {
+            return false;
+        }
+
+        if( !isReady )
+            return false;
+
+        Log.d("touch", "in keydown event in gameboard ...");
         if(keyCode == OuyaController.BUTTON_DPAD_RIGHT ) {
+            if( (mMuncherBoardLocation.X + 1) >= pieces.length)
+                return false;
+            mMuncherBoardLocation.X += 1;
+            moveMuncher(pieces[mMuncherBoardLocation.X][mMuncherBoardLocation.Y]);
             return true;
         } else if (keyCode == OuyaController.BUTTON_DPAD_LEFT ) {
+            if( (mMuncherBoardLocation.X - 1) < 0)
+                return false;
+            mMuncherBoardLocation.X -= 1;
+            moveMuncher(pieces[mMuncherBoardLocation.X][mMuncherBoardLocation.Y]);
             return true;
         } else if (keyCode == OuyaController.BUTTON_DPAD_DOWN ) {
-
+            if( (mMuncherBoardLocation.Y - 1) < 0)
+                return false;
+            mMuncherBoardLocation.Y -= 1;
+            moveMuncher(pieces[mMuncherBoardLocation.X][mMuncherBoardLocation.Y]);
+            return true;
         }
         else if ( keyCode == OuyaController.BUTTON_DPAD_UP ) {
+            if( (mMuncherBoardLocation.Y + 1) >=  pieces[0].length)
+                return false;
+            mMuncherBoardLocation.Y += 1;
+            moveMuncher(pieces[mMuncherBoardLocation.X][mMuncherBoardLocation.Y]);
+            return true;
+        }
+        else if ( keyCode == OuyaController.BUTTON_O) {
+            eatNote(pieces[mMuncherBoardLocation.X][mMuncherBoardLocation.Y]);
 
+            if( isLevelComplete() )
+            {
+                Log.w("touch", "GAME OVER!!!");
+                mLevel.levelComplete();
+            }
+            return true;
         }
 
+        Log.d("touch", "moving the board piece...");
         return false;
     }
 	
@@ -166,12 +234,14 @@ public class Gameboard extends Rectangle implements MusicMuncherDefines, IOuyaCo
 	}
 	
 	private void moveMuncher( final BoardPiece pieceTouched ) {
+        Log.d("touch", "moving muncher to board index from piece touched " + pieceTouched.boardIndexLocation().X + "," + pieceTouched.boardIndexLocation().Y);
+        mMuncherBoardLocation = pieceTouched.boardIndexLocation();
+        Log.d("touch", "moving muncher to board index " + mMuncherBoardLocation.X + "," + mMuncherBoardLocation.Y);
 		mLevel.mMuncher.setPosition(pieceTouched.getX(), pieceTouched.getY());
 	}
 	
 	private String chooseNote( final String scale ) {
 		if( random.nextInt(1) == 1 ){
-            mTotalNoteCount += 1;
             return ScaleRepository.RandomNote(scale);
         }
 
@@ -186,7 +256,11 @@ public class Gameboard extends Rectangle implements MusicMuncherDefines, IOuyaCo
 	}
 
     private final BoardPiece randomBoardPiece() {
-        Point cellIndex = new Point( random.nextInt(pieces.length), random.nextInt(pieces[0].length));
+        Log.d("touch", "pieces column length: " + pieces.length + "   row length"  + pieces[0].length);
+        Vector2 cellIndex = new Vector2( random.nextInt(pieces.length), random.nextInt(pieces[0].length));
+
+        Log.d("touch", "random index column: " + cellIndex.X + "   row "  + cellIndex.Y);
+
         return pieces[cellIndex.X][cellIndex.Y];
     }
 
